a187cfc1c1bdff0e23c402db8518bd0a2e09ab99
LIANG LI <llbuaa2015@gmail.com>
2024-03-01 12:55:13 +0800

    Update to myself

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-print("Welcome Anthony! How are you doing today?")
+print("Welcome LILIANG! How are you doing today?")
 
 vim.g.mapleader = " "
 
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/anthdm/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/anthdm/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/anthdm/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/anthdm/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/anthdm/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/home/deepdata/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/deepdata/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/deepdata/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/deepdata/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/deepdata/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,128 +76,128 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["cmp-nvim-lua"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
     url = "https://github.com/hrsh7th/cmp-nvim-lua"
   },
   ["cmp-path"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/cmp-path",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
   },
   cmp_luasnip = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
   ["friendly-snippets"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/friendly-snippets",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
   grail = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/grail",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/grail",
     url = "https://github.com/chama-chomo/grail"
   },
   ["gruvbox.nvim"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/gruvbox.nvim",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/gruvbox.nvim",
     url = "https://github.com/ellisonleao/gruvbox.nvim"
   },
   ["lsp-zero.nvim"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
     url = "https://github.com/VonHeikemen/lsp-zero.nvim"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["naysayer88.vim"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/naysayer88.vim",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/naysayer88.vim",
     url = "https://github.com/jhlgns/naysayer88.vim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-comment"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/nvim-comment",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/nvim-comment",
     url = "https://github.com/terrortylor/nvim-comment"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = false,
     needs_bufread = false,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/opt/nvim-web-devicons",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/opt/nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["toggleterm.nvim"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/toggleterm.nvim",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/toggleterm.nvim",
     url = "https://github.com/akinsho/toggleterm.nvim"
   },
   ["vim-go"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/vim-go",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/vim-go",
     url = "https://github.com/fatih/vim-go"
   },
   ["vim-handmade-hero"] = {
     loaded = true,
-    path = "/home/anthdm/.local/share/nvim/site/pack/packer/start/vim-handmade-hero",
+    path = "/home/deepdata/.local/share/nvim/site/pack/packer/start/vim-handmade-hero",
     url = "https://github.com/CreaturePhil/vim-handmade-hero"
   }
 }
