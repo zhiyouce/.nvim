e79b7a110dd5eb1fec57f6613db4dcddad8ab99a
LIANG LI <llbuaa2015@gmail.com>
2024-03-05 22:16:51 +0800

    remove lsp ts rust eslint

--- init.lua
+++ init.lua
@@ -108,10 +108,10 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-	"tsserver",
+--	"tsserver",
 	"gopls",
-	"eslint",
-	"rust_analyzer",
+--	"eslint",
+--	"rust_analyzer",
 })
 
 lsp.set_preferences({
